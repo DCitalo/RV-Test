d126147f2a559504326c3a00b0d5667c65a30606
DCitalo <cd_italo@hotmail.com>
2017-10-31 01:50:11 -0200

    Layout 0.0.3

---
+++ src/javascripts/default.js
@@ -0,0 +1,83 @@
+$(".datepicker-here").click(function() {
+	var DateIn      = $(".-range-from-").attr('data-date'),
+		MonthIn    = $(".-range-from-").attr('data-month'),
+		YearIn     = $(".-range-from-").attr('data-year'),
+		DateOut    = $(".-range-to-").attr('data-date'),
+		MonthOut    = $(".-range-to-").attr('data-month'),
+		YearOut    = $(".-range-to-").attr('data-year');
+	if (MonthIn == "1") {
+		MonthIn = "January";
+	}
+	if (MonthIn == "2") {
+		MonthIn = "February";
+	}
+	if (MonthIn == "3") {
+		MonthIn = "March";
+	}
+	if (MonthIn == "4") {
+		MonthIn = "April";
+	}
+	if (MonthIn == "5") {
+		MonthIn = "May";
+	}
+	if (MonthIn == "6") {
+		MonthIn = "June";
+	}
+	if (MonthIn == "7") {
+		MonthIn = "July";
+	}
+	if (MonthIn == "8") {
+		MonthIn = "August";
+	}
+	if (MonthIn == "9") {
+		MonthIn = "September";
+	}
+	if (MonthIn == "10") {
+		MonthIn = "October";
+	}
+	if (MonthIn == "11") {
+		MonthIn = "November";
+	}
+	if (MonthIn == "12") {
+		MonthIn = "December";
+	}
+	if (MonthOut == "1") {
+		MonthOut = "January";
+	}
+	if (MonthOut == "2") {
+		MonthOut = "February";
+	}
+	if (MonthOut == "3") {
+		MonthOut = "March";
+	}
+	if (MonthOut == "4") {
+		MonthOut = "April";
+	}
+	if (MonthOut == "5") {
+		MonthOut = "May";
+	}
+	if (MonthOut == "6") {
+		MonthOut = "June";
+	}
+	if (MonthOut == "7") {
+		MonthOut = "July";
+	}
+	if (MonthOut == "8") {
+		MonthOut = "August";
+	}
+	if (MonthOut == "9") {
+		MonthOut = "September";
+	}
+	if (MonthOut == "10") {
+		MonthOut = "October";
+	}
+	if (MonthOut == "11") {
+		MonthOut = "November";
+	}
+	if (MonthOut == "12") {
+		MonthOut = "December";
+	}
+
+	console.log(MonthIn +" "+ DateIn +", "+ YearIn);
+	console.log(MonthOut +" "+ DateOut +", "+ YearOut);
+});
--- src/javascripts/i18n/datepicker.en.js
+++ src/javascripts/i18n/datepicker.en.js
@@ -1,7 +1,7 @@
 ;(function ($) { $.fn.datepicker.language['en'] = {
     days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    daysMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
     months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
     monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     today: 'Today',
